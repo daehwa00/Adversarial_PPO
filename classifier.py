0a23df15d92607a870c1f37a5f0da2a95abf08d3
daehwa <daehwa001210@gmail.com>
2024-03-13 13:36:30 +0900

    fix code to fit ImageNet Dataset

--- classifier.py
+++ classifier.py
@@ -4,7 +4,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 import torchvision
-from torchvision import transforms
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 
 import os
 from collections import OrderedDict
@@ -14,163 +16,12 @@ from collections import OrderedDict
 transform = transforms.Compose(
     [
         transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ]
 )
 
-trainset = torchvision.datasets.CIFAR10(
-    root="./data", train=True, download=False, transform=transform
-)
-trainloader = DataLoader(trainset, batch_size=512, shuffle=False, num_workers=4)
-
-testset = torchvision.datasets.CIFAR10(
-    root="./data", train=False, download=False, transform=transform
-)
-testloader = DataLoader(testset, batch_size=512, shuffle=False, num_workers=4)
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-        )
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=1, padding=1, bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes,
-                    self.expansion * planes,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(self.expansion * planes),
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(
-            planes, planes, kernel_size=3, stride=stride, padding=1, bias=False
-        )
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(
-            planes, self.expansion * planes, kernel_size=1, bias=False
-        )
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_planes,
-                    self.expansion * planes,
-                    kernel_size=1,
-                    stride=stride,
-                    bias=False,
-                ),
-                nn.BatchNorm2d(self.expansion * planes),
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-
-class FeatureResNet(nn.Module):
-    def __init__(self, block, num_blocks):
-        super(FeatureResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
-        out = out.view(out.size(0), -1)
-        return out
-
-
-def FeatureResNet18():
-    return FeatureResNet(BasicBlock, [2, 2, 2, 2])
-
-
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+dataset = datasets.ImageFolder("path/to/imagenet/validation", transform=transform)
+dataloader = DataLoader(dataset, batch_size=64, shuffle=True)
 
 
 # CIFAR10 클래스
@@ -229,23 +80,13 @@ def evaluate_and_filter_dataset(loader, model, device, env_batch):
     return accuracy, filtered_loader
 
 
-def load_model_and_filtered_loader(
-    device, env_batch, model_path="./pre-trained models/cifar10_resnet18_best.pth"
-):
+def load_model_and_filtered_loader(device, env_batch):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     # 모델 로딩과 초기화
-    model = ResNet18().to(device)
-    if model_path and os.path.isfile(model_path):
-        state_dict = torch.load(model_path, map_location=device)["net"]
-        model.load_state_dict(OrderedDict((k[7:], v) for k, v in state_dict.items()))
-        print("Successfully loaded classifier model.")
-    else:
-        print("Model path is invalid. Exiting.")
-        return None, None
-
+    model = models.ResNeXt101_32X8D_Weights(pretrained=True)
     # 데이터셋으로부터 DataLoader 생성
-    loader = DataLoader(trainset, batch_size=env_batch, shuffle=True, num_workers=4)
+    loader = DataLoader(dataset, batch_size=env_batch, shuffle=True, num_workers=4)
 
     # 정확도 평가 및 필터링된 데이터셋 생성
     accuracy, filtered_loader = evaluate_and_filter_dataset(
